71cc4d0b4306e0f5462b026dfe922f268e60c0e6
lirummirul <89711820+lirummirul@users.noreply.github.com>
2022-07-05 18:39:50 +0400

    Initial Commit

---
+++ Meditation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meditation
+//
+//  Created by Лада on 05.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
